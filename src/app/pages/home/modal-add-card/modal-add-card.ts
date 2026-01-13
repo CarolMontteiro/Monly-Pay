36822715e52e15bd380cc70a1801d0c0c276f463
CarolMontteiro <carolmonteiro1206@gmail.com>
2026-01-13 14:26:27 -0300

    Recebemos um valor do input, geramos um ID baseado em timestamp, limpamos o input e damos foco novamente no input

--- src/app/pages/home/modal-add-card/modal-add-card.ts
+++ src/app/pages/home/modal-add-card/modal-add-card.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-modal-add-card',
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './modal-add-card.css',
 })
 export class ModalAddCard {
-
   @Input() show!: boolean;
   @Output() close = new EventEmitter<void>();
 
@@ -16,4 +15,39 @@ export class ModalAddCard {
     this.close.emit();
   }
   
+  #cdr = inject(ChangeDetectorRef);
+  
+  @ViewChild("inputText") public inputText!: ElementRef;
+
+  @Output() public outputCardList = new EventEmitter<{
+    id: string;
+    checked: boolean;
+    value: string;
+  }>();
+
+  public addCard(value: string) {
+    if(value) {
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = '';
+
+      const dataAtual = new Date();
+      const timestamp = dataAtual.getTime();
+      const id = `ID ${timestamp}`
+
+      this.outputCardList.emit({
+        id,
+        checked: false,
+        value
+      })
+
+      console.log({
+        id,
+        checked: false,
+        value
+      });
+      
+      
+      return this.inputText.nativeElement.focus();
+    }
+  }
 }
