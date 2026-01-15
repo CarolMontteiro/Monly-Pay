14e147ef0d8df1ee24122fd064bcf0aeec2f3801
CarolMontteiro <carolmonteiro1206@gmail.com>
2026-01-15 14:39:15 -0300

    Atualização de código: Usando API nova baseada em signals, e Adicionando dados no LocalStrorage

--- src/app/pages/home/modal-add-card/modal-add-card.ts
+++ src/app/pages/home/modal-add-card/modal-add-card.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, input, output, viewChild,} from '@angular/core';
+import { ICardItem } from '../interface/ICardItem.interface';
 
 @Component({
   selector: 'app-modal-add-card',
@@ -8,46 +9,34 @@ import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, 
   styleUrl: './modal-add-card.css',
 })
 export class ModalAddCard {
-  @Input() show!: boolean;
-  @Output() close = new EventEmitter<void>();
+  show = input<boolean>(false);
+  close = output<void>();
 
   closeModal() {
     this.close.emit();
   }
   
-  #cdr = inject(ChangeDetectorRef);
-  
-  @ViewChild("inputText") public inputText!: ElementRef;
+  public inputText = viewChild<ElementRef<HTMLInputElement>>("inputText");
 
-  @Output() public outputCardList = new EventEmitter<{
-    id: string;
-    checked: boolean;
-    value: string;
-  }>();
-
-  public addCard(value: string) {
-    if(value) {
-      this.#cdr.detectChanges();
-      this.inputText.nativeElement.value = '';
-
-      const dataAtual = new Date();
-      const timestamp = dataAtual.getTime();
+  public outputAddCardList = output<ICardItem>();
+  public focusAndAddCard(value: string) {
+    if(!value.trim()) return; {
+     
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
       const id = `ID ${timestamp}`
 
-      this.outputCardList.emit({
+      this.outputAddCardList.emit({
         id,
         checked: false,
         value
       })
-
-      console.log({
-        id,
-        checked: false,
-        value
-      });
       
-      
-      return this.inputText.nativeElement.focus();
+      const input = this.inputText()?.nativeElement;
+      if(input) {
+        input.value = '';
+        input.focus();
+      }
     }
   }
 }
